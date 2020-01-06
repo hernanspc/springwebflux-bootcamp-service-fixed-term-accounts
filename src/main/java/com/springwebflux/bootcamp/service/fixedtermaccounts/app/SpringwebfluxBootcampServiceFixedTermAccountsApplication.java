e48d8cfae3e3492fd88c87f77062abcd24b6e605
Hernan <iphone1198@icloud.com>
2020-01-06 10:06:54 -0500

    first commit

---
+++ src/main/java/com/springwebflux/bootcamp/service/fixedtermaccounts/app/SpringwebfluxBootcampServiceFixedTermAccountsApplication.java
@@ -0,0 +1,13 @@
+package com.springwebflux.bootcamp.service.fixedtermaccounts.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebfluxBootcampServiceFixedTermAccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebfluxBootcampServiceFixedTermAccountsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springwebflux/bootcamp/service/fixedtermaccounts/app/SpringwebfluxBootcampServiceFixedTermAccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springwebflux.bootcamp.service.fixedtermaccounts.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringwebfluxBootcampServiceFixedTermAccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
